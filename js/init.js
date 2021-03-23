286aedf35572c3e7e9c660cec0c3efec4b4d9499
autenticar41ALR <81251768+autenticar41ALR@users.noreply.github.com>
2021-03-23 10:40:06 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,9 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyBO38Suz5BdmeqS0QkX-vi4lMuKXyd3uVA",
+    authDomain: "autenticar41alr-8d3de.firebaseapp.com",
+    projectId: "autenticar41alr-8d3de",
+    storageBucket: "autenticar41alr-8d3de.appspot.com",
+    messagingSenderId: "1061733012776",
+    appId: "1:1061733012776:web:153975dd7f40c7e8baac1d",
+    measurementId: "G-M683THN5C3"
+    });
